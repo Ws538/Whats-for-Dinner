06cdc6e0f8980963a6e4b570edf36dc0e85cef84
Ws538 <williamsu668@gmail.com>
2023-01-03 18:37:07 +1100

    new update

--- assets/FoodBtn.js
+++ assets/FoodBtn.js
@@ -1,17 +1,19 @@
 var foodButton = document.querySelector("#FoodBtn");
 var content = document.querySelector(".content");
 var foodQuestion = document.querySelector("#foodQuestion");
-
+var burgerImage = document.querySelector("#burgerImage");
+var healthyBtn = document.querySelector("#healthyBtn");
 
 //render Food Questions
+burgerImage.classList.remove("hide");
+
 function startFoodQuestions() {
-    content.classList.add("hide");
-    foodQuestion.classList.remove("hide")
-    
-    
-  }
+  content.classList.add("hide");
+  foodQuestion.classList.remove("hide");
+}
 
+function openHealthyHTML() {
+  window.location.href = "./assets/HealthyFoods/healthy.html"
+}
 
-
-
-foodButton.addEventListener("click", startFoodQuestions)
+foodButton.addEventListener("click", startFoodQuestions);
