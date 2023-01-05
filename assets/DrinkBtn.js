2d379dd46b8e1986058e3ee48aa94466479d2cfd
VC <victoriarccook@gmail.com>
2023-01-05 19:42:14 +1100

    add drink functionality to DrinkBtn.js

--- assets/DrinkBtn.js
+++ assets/DrinkBtn.js
@@ -1,23 +1,30 @@
-var foodButton = document.querySelector("#FoodBtn");
+var drinkButton = document.querySelector("#DrinkBtn");
 var content = document.querySelector(".content");
-var foodQuestion = document.querySelector("#foodQuestion");
+var drinkQuestion1 = document.querySelector("#drinkQuestion1");
+var drinkQuestion2 = document.querySelector('#drinkQuestion2');
 var burgerImage = document.querySelector("#burgerImage");
-var healthyBtn = document.querySelector("#healthyBtn");
+// var healthyBtn = document.querySelector("#healthyBtn");
 
-//render Food Questions
+//hide burger image
 burgerImage.classList.remove("hide");
 
-function startFoodQuestions() {
+//show drink question 1
+function startDrinkQuestions() {
   content.classList.add("hide");
-  foodQuestion.classList.remove("hide");
+  drinkQuestion1.classList.remove("hide");
 }
 
-function openHealthyHTML() {
-  window.location.href = "./assets/HealthyFoods/healthy.html"
+//show drink question 2
+function showDrinkQuestion2() {
+    content.classList.add("hide");
+    drinkQuestion1.classList.add("hide");
+    drinkQuestion2.classList.remove("hide");
 }
 
-function openJunkHTML() {
-  window.location.href = "./assets/JunkFoods/Junk.html"
+//show drink options based on previous two user choices
+function showDrinkOptions() {
+//   window.location.href = "./assets/JunkFoods/Junk.html"
 }
 
-foodButton.addEventListener("click", startFoodQuestions);
+//add event listener
+drinkButton.addEventListener("click", startDrinkQuestions);
