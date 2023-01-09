407b62d86b756d5274f5d1c9fcb123cd23e39235
VC <victoriarccook@gmail.com>
2023-01-09 20:18:26 +1100

    drinkAPI js file edits

--- assets/drinkAPI.js
+++ assets/drinkAPI.js
@@ -2,6 +2,39 @@ let params = (new URL(document.location)).searchParams;
 let drinkName = params.get('name');
 console.log(drinkName);
 url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkName;
+const drinkImgDisplay = document.querySelector('img');
+const drinkNameDisplay = document.querySelector('h1');
+const drinkIngredientsDisplay = document.querySelector('ol');
+const drinkInstructionsDisplay = document.querySelector('p');
+
+function putFetchInFunction() {
+    fetch(url)
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then((data) => {
+        displayRecipe(data)
+      });
+    // .then((data) => {
+    //     // displayRecipe();
+    //   });
+} 
+
+// function to display result by populating page with recipe details
+function displayRecipe(drinks) {
+    drinkImgDisplay.setAttribute('src', drinks.drinks[0].strDrinkThumb);
+    drinkNameDisplay.innerHTML = drinks.drinks[0].strDrink;
+    drinkInstructionsDisplay.innerHTML = drinks.drinks[0].strInstructions;
+    //drinkIngredientsDisplay = drinks.drinks[0].strIngredient1 (loop through numbers +1 until null?)
+}
+
+function listIngredients() {
+    for (let i = 0; i < ; i++)
+    const ingredientItem = document.createElement('list item');
+    ingredientItem.innerHTML = drinks.drinks[0].strIngredient1;
+    drinkIngredientsDisplay.appendChild(ingredientItem);
+}
+
+putFetchInFunction();
 
 //call API function by passing in ID of the tile that was clicked
 // function setupUrl(event) {
@@ -10,27 +43,3 @@ url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkName;
 //     url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + chosenDrink;
 //     putFetchInFunction();
 // }
-
-function putFetchInFunction() {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .then((data) => {
-        displayRecipe();
-      });
-    // displayRecipe();
-} 
-
-putFetchInFunction();
-
-
-// function to display result by opening new html and printing result
-function displayRecipe() {
-    window.location.href = './drink-recipe.html?name=paloma';
-    // send query like drink name = 
-    //populate bootstrap card with drink name, image and recipe
-    //drink name = drinks[0].strDrink
-    //drink image = drinks[0].strDrinkThumb
-    //drink ingredients = drinks[0].strIngredient1 (loop through numbers +1 until null?)
-    //drink instructions = drinks[0].strInstructions
-}
