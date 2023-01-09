700fcae6a3a29d6a16af4690696fd83eaab334a0
Sam Sweeney <samsweeneyy@gmail.com>
2023-01-09 13:06:19 +1100

    Added search bar functionality in search page

--- assets/Search/search.js
+++ assets/Search/search.js
@@ -1,5 +1,18 @@
 const baseurl =
   "https://api.edamam.com/api/recipes/v2?app_id=86bfcee4&app_key=28ee446263661df1201ba54d78bd5e1d&type=public";
+
+const searchButton = document.getElementById("search-btn");
+const searchBar = document.getElementById("search");
+
+searchBar.addEventListener("focus", (e) => {
+  searchButton.style.opacity = "1";
+});
+searchBar.addEventListener("focusout", (e) => {
+  if (!e.target.value.length) {
+    searchButton.style.opacity = "0";
+  }
+});
+
 const animationObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
@@ -19,11 +32,11 @@ const animateCards = (cards) => {
 };
 
 const showError = async (message) => {
-  const errorDiv = document.querySelector('.error');
+  const errorDiv = document.querySelector(".error");
   errorDiv.childNodes[1].innerText = message;
   errorDiv.style.transform = "translateY(0)";
   errorDiv.style.opacity = 1;
-  await new Promise(r=>setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   errorDiv.style.transform = "translateY(-200px)";
   errorDiv.style.opacity = 0;
 };
@@ -66,15 +79,13 @@ const showRecipes = (data) => {
           }" class="card-img-top" alt="">
           <div class="card-body">
             <h5 class="card-title">${recipe.recipe.label}</h5>
-            <div class="d-flex justify-content-between">
+        
             <a href="${
               window.location.origin +
               `/assets/Recipe/recipe.html?recipe=${recipeId}`
             }" class="btn" style="background-color: #A83E3C; color: white;">Details</a>
-            <button class="save" data-id="${recipeId}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
-                <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
-              </svg></button>
-        </div>
+          
+     
           </div>
         </div>
         `;
@@ -92,22 +103,20 @@ const observer = new IntersectionObserver((entries) => {
           .then(async (data) => {
             showRecipes(data);
             lastDiv = document.querySelector(".card:last-child");
-            if (lastDiv){
+            if (lastDiv) {
               observer.observe(lastDiv);
             }
             observer.unobserve(entry.target);
             const allCards = document.querySelectorAll(".card");
             await new Promise((r) => setTimeout(r, 750));
             animateCards(allCards);
-          })
+          });
       }
     }
   });
 });
 
-window.onload = async function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  const searchParam = urlParams.get("q");
+const getAndShowRecipes = async (searchParam) => {
   const { recipes, error } = await getRecipes(baseurl + `&q=${searchParam}`);
   if (error) {
     await showError("Server error");
@@ -115,9 +124,31 @@ window.onload = async function () {
   }
   showRecipes(recipes);
   lastDiv = document.querySelector(".card:last-child");
-  if (lastDiv){
+  if (lastDiv) {
     observer.observe(lastDiv);
   }
-  const allCards = document.querySelectorAll('.card');
+  const allCards = document.querySelectorAll(".card");
   animateCards(allCards);
 };
+
+window.onload = async function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const searchParam = urlParams.get("q");
+  getAndShowRecipes(searchParam);
+};
+const saveSearchToLS = (search) => {
+  const ls = JSON.parse(localStorage.getItem("recent"));
+  if (ls) {
+    localStorage.setItem("recent", JSON.stringify([...ls, search]));
+  } else {
+    localStorage.setItem("recent", JSON.stringify([search]));
+  }
+};
+searchButton.addEventListener("click", async (e) => {
+  const searchParam = searchBar.value;
+  const url = new URL(window.location);
+  url.searchParams.set("q", searchParam);
+  window.history.pushState(null, "", url.toString());
+  saveSearchToLS(searchParam);
+  getAndShowRecipes(searchParam);
+});
