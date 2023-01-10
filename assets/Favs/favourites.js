2e5d8736c62a3d94c21d63724a0b38f45fcbeac4
Sam Sweeney <samsweeneyy@gmail.com>
2023-01-10 14:26:02 +1100

    Added ability to delete favourites

--- assets/Favs/favourites.js
+++ assets/Favs/favourites.js
@@ -1,9 +1,36 @@
-const ul = document.getElementById('fav-list');
-const ls = JSON.parse(localStorage.getItem('favourites'));
-if (ls) {
-  ul.innerHTML = `
-    ${ls.map(item=>{
-      return `<li><a href="${window.location.origin}/assets/Recipe/recipe.html?recipe=${item.id}">${item.name}</a></li>`;
-    }).join('')}
-  `
+
+function displayFavourites() {
+  const ul = document.getElementById('fav-list');
+  const ls = JSON.parse(localStorage.getItem('favourites'));
+  if (ls) {
+    ul.innerHTML = `
+      ${ls.map(item=>{
+        return `<li><a href="${window.location.origin}/assets/Recipe/recipe.html?recipe=${item.id}">${item.name}</a> <button data-id=${item.id} class="delete-btn">Delete</button></li>`;
+      }).join('')}
+    `
+  }  
 }
+function removeFavouriteLS(id) {
+  const ls = JSON.parse(localStorage.getItem('favourites'));
+  if (ls) {
+    const filterdLS = ls.filter(item=>{
+      return item.id !== id;
+    })
+    localStorage.setItem('favourites', JSON.stringify(filterdLS));
+  }
+}
+
+function initButtons() {
+  const buttons = document.querySelectorAll('.delete-btn');
+  const ul = document.getElementById('fav-list')
+  buttons.forEach(button=>{
+    button.addEventListener('click', (e) => {
+      ul.removeChild(e.target.parentNode)
+      removeFavouriteLS(e.target.dataset.id)
+    })
+  })
+}
+
+
+displayFavourites();
+initButtons();
